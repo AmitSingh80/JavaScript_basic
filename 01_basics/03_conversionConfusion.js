8979f22730264ebd1442e2a2d5de1159071e3d75
AMIT KR SINGH <98234829+AmitSingh80@users.noreply.github.com>
2024-04-19 11:48:29 +0000

    Datatype conversion

---
+++ 01_basics/03_conversionConfusion.js
@@ -0,0 +1,40 @@
+
+
+// let score=33
+// let score="33abc"
+// let score=null
+// let score=undefined
+let score="amit"
+// let score=true
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber= Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+//"33"==> 33
+// "33abc"==>NaN
+// null==> 0
+//undefined == NaN
+//"amit"==NaN
+// true== 1 , false ===0
+
+let isLoggedIn=1
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+//1==true;  0==false
+//"" == false
+// " amit" == true
+
+
+
+let someNumber=33
+let stringNumber=String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
