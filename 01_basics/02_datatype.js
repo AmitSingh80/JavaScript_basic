438be30892b9d5ef836d8c1d4dcf4f43d2d4be3d
AMIT KR SINGH <98234829+AmitSingh80@users.noreply.github.com>
2024-04-19 10:57:33 +0000

    datatype

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,41 @@
+"use strict" // treat all JS code as new version
+
+
+// alert("hello")
+// alert(3+3)  // we are using nodejs , not brower
+
+console.log(3-3);     //code readable hona chahiye
+console.log(" amit");
+
+
+
+// string add ,sub, mul, div
+
+console.log("3"+3);  // if we add in string we get result 33
+// console.log("3"-9);   // run get result 0
+console.log("3"*3);   // run get result 9
+console.log("3"/3);    // run get result 1
+
+//important
+console.log("3"-9); // if we subtract in string if get
+
+// datatype
+
+//number=2 to power 53
+// string=""
+// boolean= true/false
+//null= standlone value
+// undefine=
+//symbol= unique
+let age=23
+let name="amit"
+let string;
+let isLoggedIn=false
+
+//object
+
+
+console.log(typeof"amit");
+console.log(typeof age);
+console.log(typeof null);     //object
+console.log(typeof undefined);  // undefined
