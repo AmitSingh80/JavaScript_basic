3e4082ced474fa472648a5d82e62c8e0a78c048d
AMIT KR SINGH <98234829+AmitSingh80@users.noreply.github.com>
2024-04-20 08:53:59 +0000

    comparision done

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,22 @@
+// console.log(2>1);
+// console.log(3<2);
+// console.log(2==2);
+
+
+// console.log("2">1);
+// console.log("02">1);
+
+
+// console.log(null >0);
+// console.log(null==0);
+// console.log(null>=0);  //true
+
+// console.log(undefined=0);  //  get 0
+// console.log(undefined<0);
+// console.log(undefined>0);
+// console.log(undefined>=0);
+
+//=====
+
+console.log("2"==4);
+
