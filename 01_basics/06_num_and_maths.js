ef22144eeb83efb64169f449b6656bd69ed44d16
AMIT KR SINGH <98234829+AmitSingh80@users.noreply.github.com>
2024-04-21 07:46:51 +0000

    math and num

---
+++ 01_basics/06_num_and_maths.js
@@ -0,0 +1,38 @@
+const score =400
+// console.log(score);
+
+const balance=new Number(100)
+// console.log(balance);
+
+
+// console.log(balance.toString());
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(3));
+
+const numberOther=23.567
+// console.log(numberOther.toPrecision(3));
+
+const hundreds= 1000000
+// console.log(hundreds.toLocaleString()); // based on international 
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+
+//++++++++++++++++ MATHS +++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-3));
+// console.log(Math.round(6.7));
+// console.log(Math.floor(3.9));
+// console.log(Math.max(5,8,9,2));
+// console.log(Math.min(1,4,5,6,7,8));
+console.log(Math.floor(Math.random()*10)+1);
+
+console.log(Math.random());
+console.log(Math.random()*10);
+
+const min=10
+const max= 30
+console.log(Math.floor(Math.random()*(max-min+1)+min));
+
+
