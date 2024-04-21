1d5f59242328ac688b16312877099922d6ef39dd
AMIT KR SINGH <98234829+AmitSingh80@users.noreply.github.com>
2024-04-21 06:32:23 +0000

    String

---
+++ 01_basics/05_string.js
@@ -0,0 +1,43 @@
+const name="amit"
+const repoCount = 5
+
+// console.log(name + repoCount + " Value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('Amithey-com')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('t'));
+
+const newString = gameName.substring(0, 4)
+// console.log(newString);
+
+const anotherString = gameName.slice(-8, 4)
+// console.log(anotherString);
+
+const newStringOne = "   hello amit   "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+// console.log(newStringOne.trimEnd());
+// console.log(newStringOne.trimStart());
+
+const url = "https://amit.com//%20.com"
+
+const userName="hey"
+
+// console.log(userName.replace('hey','hello'));
+// console.log(url.replace('%20', '-'))
+// console.log(url.includes('amit'));
+// console.log(url.includes('sundar'))
+
+// console.log(gameName.split('-'));
+
+console.log(userName.repeat(2));
+console.log(userName.__proto__);
