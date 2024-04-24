8d9c7be18408f96d42e47fafe45353a72e9aa01d
AMIT KR SINGH <98234829+AmitSingh80@users.noreply.github.com>
2024-04-24 14:14:33 +0000

    Array

---
+++ 02_arrays/01_arrays.js
@@ -0,0 +1,49 @@
+//Array
+
+
+const myArray=[0,1,2,3,4,5]
+const MyHero=["saktiman","batman"]
+
+const myarr=new Array(1,2,3)
+
+// console.log(myArray);
+// console.log(myArray[2]);
+// console.log(MyHero);
+// console.log(myarr);
+
+
+//Array Method
+// myArray.push(6)
+// myArray.pop()
+
+// myArray.unshift(1)
+// myArray.shift()
+
+// console.log(myArray.includes(5));
+// console.log(myArray.indexOf(9));
+
+
+// console.log(myArray);
+
+
+
+// const newArr=myArray.join()
+
+// console.log(myArray);
+// console.log(newArr);
+
+
+
+//slice, splice
+
+
+console.log("A",myArray);
+
+const myn1=myArray.slice(1, 3)
+console.log(myn1);
+
+
+console.log("B" ,myArray);
+const myn2=myArray.splice(1 ,3)
+console.log("C", myArray);
+console.log(myn2);
