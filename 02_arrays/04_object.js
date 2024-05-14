7e89612553a14b522e4988407e9645272ffaaff0
AMIT KR SINGH <98234829+AmitSingh80@users.noreply.github.com>
2024-05-14 18:20:45 +0000

    objects

---
+++ 02_arrays/04_object.js
@@ -0,0 +1,8 @@
+const tinderUser= new Object()  //empty object
+const tinderUser2 = {}          // empty object
+
+tinderUser.id = "123abc"
+tinderUser.name = "Amit"
+tinderUser.isLoggedIn = false
+
+console.log(tinderUser);
