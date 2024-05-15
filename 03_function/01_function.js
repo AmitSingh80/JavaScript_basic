287d4d4eb33215827caa40233f96e0b9bba0bbbf
AMIT KR SINGH <98234829+AmitSingh80@users.noreply.github.com>
2024-05-15 12:11:08 +0000

    function done

--- 03_function/01_function.js
+++ 03_function/01_function.js
@@ -4,4 +4,43 @@ console.log('i');
 console.log('m');
 console.log('t');
 }
-sayMyName()
+// sayMyName()
+
+// function addTwoNumbers(number1, number2){    // define parameters
+//     console.log(number1+ number2);
+// }
+//  addTwoNumbers(4,5)   // value pass argument
+
+
+
+function addTwoNumbers(number1, number2){    // define parameters
+    let result= number1+ number2
+    // console.log("amit");
+    return result
+    //below this not execute
+    // console.log("amit");
+}
+  const result=  addTwoNumbers(4,5)   // value pass argument
+//    console.log("result: " ,result);
+
+// another way to return the value
+   function sumTWoNumber (num1,num2){
+     return num1+num2
+   }
+   const Sum=sumTWoNumber(3,4)
+//    console.log("result: ",Sum);
+
+
+
+function loginUser(userName){
+    // if(userName===undefined) 
+        if(!userName)              //better syntax or another way
+        {
+      
+          console.log("please enter username");
+     return;
+    }
+    return `${userName} just loggedIn`
+}
+//  console.log(loginUser("amit"));
+console.log(loginUser());  //undefined print
