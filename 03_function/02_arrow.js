30f99fa2325da46249dd5c46a0cbcb5737d91803
AMIT KR SINGH <98234829+AmitSingh80@users.noreply.github.com>
2024-06-06 19:52:49 +0000

    arrow

--- 03_function/02_arrow.js
+++ 03_function/02_arrow.js
@@ -16,12 +16,36 @@ const userName={
 // ============================================
 
 
-function chai(){
-     let userName="hey";
-     console.log(this);
-}
-chai()
+// function chai(){
+//      let userName="hey";
+//      console.log(this);
+// }
+// chai()
+
+// ==================================  we can use this in both arrow function and function 
+// const chai = ()=>{
+//    let useraname = 'amit'
+//    console.log(this);
+// }
+
+// chai()                 
+
+
+ 
+// const addTwo=(num1,num2) => {
+//        return num1+num2
+// }
+// explicitly return if we write { } then we use return keyword
 
 
 
+//implicit return  we not write { }
+// const addTwo=(num1,num2) => num1+num2
+// const addTwo=(num1,num2) => ( num1+num2)   //code will be single line 
 
+
+// object call
+const addTwo=(num1,num2) => ({username:'amit'})   //code will be single line 
+
+
+console.log(addTwo(3,5));
