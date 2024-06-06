d7751619e5b4072d0ffd67d098fb6a8695fe3e4c
AMIT KR SINGH <98234829+AmitSingh80@users.noreply.github.com>
2024-06-06 12:29:16 +0000

    scope done

--- 04_scope/01_scope.js
+++ 04_scope/01_scope.js
@@ -26,11 +26,42 @@ function one(){
     const userName=" amit"
     function two(){
         const website ="youtube"
-        console.log(userName);
-        console.log(website);
+        // console.log(userName);
+        // console.log(website);
     }
     // console.log(website);
     two()
 }
 one()
+// ======================================
 
+if(true){
+    const userName=" thor"
+    if(userName==" thor"){
+        const website=" crome"
+        // console.log(userName + website);
+        // console.log(userName.concat(" Amit"));
+    }
+    // console.log(website); // error
+}
+// console.log(userName); //error
+
+
+/////////////////////////////////////////
+// interesting example
+
+function addOne (num){
+    return num +1
+    // console.log(num);
+}
+console.log(addOne(5));
+
+// ========================================
+// hosting v bolte hai
+
+// addtwo(6)             // cannot access addtwo before initialization 
+const addtwo = function (num){
+    return num +2
+}
+
+console.log(addtwo(6))
