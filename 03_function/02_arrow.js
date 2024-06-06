651ab35f4439945a4eda60a9b7c3cecc6c36a513
AMIT KR SINGH <98234829+AmitSingh80@users.noreply.github.com>
2024-06-06 13:17:33 +0000

    done

--- 03_function/02_arrow.js
+++ 03_function/02_arrow.js
@@ -3,12 +3,25 @@ const userName={
  price:90,
 
   welocomeMessage: function(){
-       console.log(`${this.useraname}, welcome to website`);
-       
+     //   console.log(`${this.useraname}, welcome to website`);
+     //   console.log(this);
   }
 }
 // userName.welocomeMessage()
-userName.useraname="Amit singh"
-userName.welocomeMessage()
+// userName.useraname="Amit singh"
+// userName.welocomeMessage()
+
+// console.log(this);
+
+// ============================================
+
+
+function chai(){
+     let userName="hey";
+     console.log(this);
+}
+chai()
+
+
 
 
