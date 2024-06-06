9541f27e1b1bdb8378c9b13f4873431a411395da
AMIT KR SINGH <98234829+AmitSingh80@users.noreply.github.com>
2024-06-06 13:00:33 +0000

    this

---
+++ 03_function/02_arrow.js
@@ -0,0 +1,14 @@
+const userName={
+ useraname:" amit",
+ price:90,
+
+  welocomeMessage: function(){
+       console.log(`${this.useraname}, welcome to website`);
+       
+  }
+}
+// userName.welocomeMessage()
+userName.useraname="Amit singh"
+userName.welocomeMessage()
+
+
