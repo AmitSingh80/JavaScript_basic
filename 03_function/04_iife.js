dc4eac716989d1678bc37c8592ccb43c767bd6b3
AMIT KR SINGH <98234829+AmitSingh80@users.noreply.github.com>
2024-06-07 18:32:46 +0000

    IIFE

--- 03_function/04_iife.js
+++ 03_function/04_iife.js
@@ -1,11 +1,22 @@
 // Immediately Invoked Function Expressions (IIFE)
 
+// why we use 
+// ANS= WE have a problem with global scope so we used IIFE to remove global scope problem 
+
 
 (function chai(){
     // named IIFE
     console.log(`DB CONNECTED`);
 })();
 
+
+// ========================
+( ()=> {
+  console.log(` hello IFFE`);
+}) (); // end with ; semicolon to run two or more IIFE function imp**
+// =================================
+
+//unName IIFE
 ( (name) => {
-    console.log(`DB CONNECTED TWO ${name}`);
-} )('amit')
+    console.log(`DB CONNECTED TWO ${name}`); //it accept variable also 
+} )('amit')  //pass the variable here
